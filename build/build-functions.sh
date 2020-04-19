10c8f90024563dc75fbc982b85b8c9da55dee94f
Greg <greg@toolstack.com>
2020-04-18 22:52:35 -0400

    Add support for device specific build functions
    To allow for differences in the build process for devices.

--- build/build-functions.sh
+++ build/build-functions.sh
@@ -24,7 +24,7 @@ function build_wos {
 	echo "Start build process for $DEVICE..."
 
 	# Move in to the build directory
-	cd ~/android/lineage
+	cd ~/android/lineage-$LOS_BUILD_VERSION
 
 	# Setup the build environment
 	source build/envsetup.sh
@@ -48,94 +48,13 @@ function build_wos {
 	echo "Build process complete for $DEVICE!"
 }
 
-function sign_wos {
-	echo "Start signing process for $DEVICE..."
-
-	# Move in to the build directory
-	cd ~/android/lineage
-
-	# Setup the build environment
-	source build/envsetup.sh
-	croot
-
+function sign_wos_target_package {
 	# Sign the APK's
 	echo "Sign target APK's..."
 	./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip signed-target_files.zip
 
 	# Create the release file
 	echo "Create release file: $PKGNAME..."
-	./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey --block --backup=true signed-target_files.zip ~/releases/ota/$PKGNAME.zip 
-
-	# Add RADIO and firmware to the update package.
-	echo "Add RADIO and FIRMWARE to the update package..."
-	cd ~/devices/$DEVICE/firmware/update
-	zip -ur ~/releases/ota/$PKGNAME.zip RADIO
-	zip -ur ~/releases/ota/$PKGNAME.zip firmware-update
-
-	# Unzip the stock updater script.
-	unzip -o ~/releases/ota/$PKGNAME.zip META-INF/com/google/android/updater-script
-
-	# Cut the orginal upate script in two at the firmware check.
-	cd ~/devices/$DEVICE/firmware/update/META-INF/com/google/android
-	csplit updater-script /oneplus.verify_modem/
-	mv xx00 updater-script-top
-	mv xx01 updater-temp
-
-	# Get rid of the firmware check from the second file.
-	tail -n +2 updater-temp > updater-script-bottom
-
-	# Clean up some of our temporary files.
-	rm updater-temp
-	rm updater-script
-
-	# Combine the old and new scripts together.
-	cat updater-script-top ~/devices/$DEVICE/firmware/update/new-updater-script ~/tasks/build/los-recovery-updater-script updater-script-bottom > updater-script
-
-	# Finish cleaning up the temporary files.
-	rm updater-script-top
-	rm updater-script-bottom
-
-	# Now add the new updater script to the release pacakage and get rid of the temporary copy.
-	cd ~/devices/$DEVICE/firmware/update
-	zip -ur ~/releases/ota/$PKGNAME.zip META-INF/com/google/android/updater-script
-	rm ~/devices/$DEVICE/firmware/update/META-INF/com/google/android/updater-script
-
-	echo "Add recovery to the release package..."
-
-	# Get the recovery image from the signed target files so we have the right signing keys in it.
-	# Use -j to drop the path as we don't need it.
-	cd ~/android/lineage
-	rm -f ~/android/lineage/recovery.img
-	unzip -j signed-target_files.zip IMAGES/recovery.img
-
-	# Add in Lineage recovery.  Use -j to drop the path as the img should be in the root of the zip.
-	zip -urj ~/releases/ota/$PKGNAME.zip ~/android/lineage/recovery.img
-
-	# Clean up.
-	rm -f ~/android/lineage/recovery.img
-
-	# Re-sign the release zip after we've updated it.
-	echo "Resign the release package..."
-	cd ~/releases/ota
-	signapk -w --min-sdk-version 28 ~/.android-certs/releasekey.x509.pem ~/.android-certs/releasekey.pk8 $PKGNAME.zip $PKGNAME-resigned.zip
-	rm $PKGNAME.zip
-	mv $PKGNAME-resigned.zip $PKGNAME.zip
-
-	# Take us back to the root.
-	cd ~/android/lineage
-
-	# Create the md5 checksum file for the release
-	echo "Create the md5 checksum..."
-	md5sum ~/releases/ota/$PKGNAME.zip > ~/releases/ota/$PKGNAME.zip.md5sum
-
-	# Grab a copy of the build.prop file
-	echo "Store the build.prop file..."
-	cp $OUT/system/build.prop ~/releases/ota/$PKGNAME.zip.prop
-
-	# Cleanup
-	echo "Store signed target files for future incremental updates..."
-	cp signed-target_files.zip ~/releases/signed_files/signed-target_files-$DEVICE-$TODAY.zip
-
-	echo "Signing process complete for $DEVICE!"
+	./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey --block --backup=true signed-target_files.zip ~/releases/ota/$PKGNAME.zip
 }
 
--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Source some functions we'll use later.
+# Source our global build functions.
 source ~/tasks/build/build-functions.sh
 
 # Set our options from the command line, even if they're wrong, we'll check them next.
@@ -17,17 +17,23 @@ fi
 
 # We cant' have more than 2 command line parameters to work.
 if [ $# -ge 3 ]; then
-    	echo "Too many parameters supplied!"
-    	help_screen
+	echo "Too many parameters supplied!"
+	help_screen
 
-    	exit 1
+	exit 1
 fi
 
 # Make sure the target device has a directory.
 if [ ! -d ~/devices/$DEVICE ]; then
-	echo "The specified device does not exist!"
-    	exit 1
+	echo "The specified device ($DEVICE) does not exist!"
+	exit 1
 fi
+
+# Source the device list.
+source ~/.WundermentOS/devices.sh
+
+# Source the build functions for this device.
+source ~/devices/$DEVICE/build/build-functions.sh
 
 # Import the destination e-mail address to send logs to.
 source ~/.WundermentOS/log-email-address.sh
@@ -35,9 +41,19 @@ source ~/.WundermentOS/log-email-address.sh
 # Create a shell variable to hold today's date in YYYYMMDD format.
 TODAY=$(date +"%Y%m%d")
 
+# Find out which version of LinageOS we're going to build for this device.
+WOS_BUILD_VAR=WOS_BUILD_VER_${DEVICE^^}
+LOS_BUILD_VERSION=${!WOS_BUILD_VAR}
+
+# Make sure we have a target LOS version.
+if [ -z "$LOS_BUILD_VERSION" ]; then
+	echo "The specified device ($DEVICE) does not have a build version!"
+	exit 1
+fi
+
 # Create shell variables for the out directry and the final package name.
-OUT=~/android/lineage/out/target/product/$DEVICE
-PKGNAME=WundermentOS-16.0-$TODAY-release-$DEVICE-signed
+OUT=~/android/lineage-$LOS_BUILD_VERSION/out/target/product/$DEVICE
+PKGNAME=WundermentOS-$LOS_BUILD_VERSION-$TODAY-release-$DEVICE-signed
 
 # Make sure we're in the build directory.
 cd ~/tasks/build
