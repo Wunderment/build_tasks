babf630a1fed8a568af06b42b76b5e89917f2c5a
Greg <greg@toolstack.com>
2020-05-09 15:36:20 -0400

    Remove Etar and restore AOSP calendar
    As linageos has reverted the AOSP changes that made the AOSP calendar useless.

--- source/source.sh
+++ source/source.sh
@@ -14,12 +14,8 @@ cd ~/tasks/source
 # Put the actual APK's in their proper directories.
 ./add-f-droid-apk.sh
 ./add-openweatherprovider-apk.sh
-./add-etar-apk.sh
 
 # Remove the googleness of AOSP/Lineage.
 ./replace-captive-portal-detection-url.sh
 ./replace-google-dns-servers.sh
 ./set-default-search-in-jelly.sh
-
-# Remove the stock calendar app for 17.1
-./remove-aosp-calendar.sh
