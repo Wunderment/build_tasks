dc5a4c5826acdb83f106a9c0bc4ca710b37a335f
Greg <greg@toolstack.com>
2020-05-18 16:29:31 -0400

    Remove global permissive mode...
    Turns out it is only a problem with Fajita and not all devices.

--- source/allow-permissive-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Note: Recovery for LOS 17.1 currently needs permissive mode set for init.
-
-cd ~/android/lineage-17.1/system/core/init
-
-sed -i 's/"-DUSER_MODE_LINUX"],/\n                "-DUSER_MODE_LINUX",\n                "-UALLOW_PERMISSIVE_SELINUX",\n                "-DALLOW_PERMISSIVE_SELINUX=1",\n            ],/' Android.bp
