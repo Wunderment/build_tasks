1c83a34c939121df8bc0a82d1abfc1c3b2396960
Greg <greg@toolstack.com>
2020-05-27 21:25:03 -0400

    Add Seedvault for 17.1 builds.

--- source/add-local-manifests.sh
+++ source/add-local-manifests.sh
@@ -9,3 +9,6 @@ for LOSPATHNAME in ~/android/lineage-*; do
 	cp ~/tasks/source/F-DroidPrivilegedExtension.xml ~/android/$LOSDIRNAME/.repo/local_manifests
 	cp ~/tasks/source/Wunderment-Vendor.xml ~/android/$LOSDIRNAME/.repo/local_manifests
 done
+
+# Seedvault only supports Android 10.
+cp ~/tasks/source/Seedvault.xml ~/android/lineage-17.1/.repo/local_manifests
--- source/lineage-17.1.common.mk
+++ source/lineage-17.1.common.mk
@@ -4,6 +4,11 @@ PRODUCT_PACKAGES += \
     F-DroidPrivilegedExtension \
     F-Droid
 
+# Add Seedvault.
+PRODUCT_PACKAGES += \
+    Seedvault \
+    BasicDreams
+
 # Remove the lineage signing keys from the recovery build, we only want ours here which get autoamtically
 # added during the signing process.
 PRODUCT_EXTRA_RECOVERY_KEYS := 
