9afabfeb89768abf34302f8c74194dcb81a56494
Greg <greg@toolstack.com>
2020-11-27 15:39:27 -0500

    Begin support for LOS 18.0.

--- source/add-sign-prebuilt.sh
+++ source/add-sign-prebuilt.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# Note: Update the building process for prebuilt images other than vendor, only supported on LOS 17.1.
+# Note: Update the building process for prebuilt images other than vendor, only supported on LOS 17.1 +.
+ANDROID_VERSIONS=( 17.1 18.0 )
 
-cd ~/android/lineage-17.1/build/make/core
+for i in "${ANDROID_VERSIONS[@]}"; do
+	echo "Patching Android $i..."
+	cd ~/android/lineage-$i/build/make/core
 
-# Check to see if we need to patch the script before doing so.
-if ! grep "BOARD_PREBUILT_IMAGES_PATH" Makefile > /dev/null; then
-        patch Makefile ~/tasks/source/core_Makefile.patch
-fi
+	# Check to see if we need to patch the script before doing so.
+	if ! grep "BOARD_PREBUILT_IMAGES_PATH" Makefile > /dev/null; then
+	        patch Makefile ~/tasks/source/core_Makefile-$i.patch
+	fi
 
-# Note: Update the signing process for prebuilt vendor and other images, only supported on LOS 17.1.
+	cd ~/android/lineage-$i/build/tools/releasetools
 
-cd ~/android/lineage-17.1/build/tools/releasetools
+	# Check to see if we need to patch the script before doing so.
+	if ! grep "updating avb hash for prebuilt vendor.img..." add_img_to_target_files.py > /dev/null; then
+	        patch add_img_to_target_files.py ~/tasks/source/add_img_to_target_files.py-$i.patch
+	fi
 
-# Check to see if we need to patch the script before doing so.
-if ! grep "updating avb hash for prebuilt vendor.img..." add_img_to_target_files.py > /dev/null; then
-        patch add_img_to_target_files.py ~/tasks/source/add_img_to_target_files.py.patch
-fi
+	# Check to see if we need to patch the script before doing so.
+	if ! grep "prebuilts_path <path to prebuild image files>" sign_target_files_apks.py > /dev/null; then
+	        patch sign_target_files_apks.py ~/tasks/source/sign_target_files_apks.py.patch
+	fi
 
-# Check to see if we need to patch the script before doing so.
-if ! grep "prebuilts_path <path to prebuild image files>" sign_target_files_apks.py > /dev/null; then
-        patch sign_target_files_apks.py ~/tasks/source/sign_target_files_apks.py.patch
-fi
-
+done
