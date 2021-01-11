e08593c095336b7e5b0849bdbd15e71add0f1e10
Greg <greg@toolstack.com>
2021-01-11 13:01:04 -0500

    Check LOS 16 exists
    before blindly adding the weather provider.

--- source/add-openweatherprovider-apk.sh
+++ source/add-openweatherprovider-apk.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Note: At this time OWP is not supported in LOS 17.1.
+# Note: At this time OWP is not supported in LOS 17.1 or later.
 
-cd ~/android/lineage-16.0/packages/apps
-mkdir OpenWeatherProvider
+# Check to see if the lineage 16.0 directory exists before doing anything else.
+if [[ -d ~/android/lineage-16.0 ]]; then
 
-cp ~/tasks/source/OpenWeatherProvider-Android.mk ~/android/lineage-16.0/packages/apps/F-Droid/Android.mk
-cp ~/android/openweathermapprovider/OpenWeatherProvider.apk ~/android/lineage-16.0/packages/apps/OpenWeatherProvider
+	cd ~/android/lineage-16.0/packages/apps
+	mkdir OpenWeatherProvider
+
+	cp ~/tasks/source/OpenWeatherProvider-Android.mk ~/android/lineage-16.0/packages/apps/F-Droid/Android.mk
+	cp ~/android/openweathermapprovider/OpenWeatherProvider.apk ~/android/lineage-16.0/packages/apps/OpenWeatherProvider
+fi
