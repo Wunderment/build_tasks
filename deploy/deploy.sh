8c13e089dc5d495456f89189713b721de1a52c1d
Greg <greg@toolstack.com>
2021-04-17 16:36:31 -0400

    Add support for "latest" and debugging info
    Create/update a symlink to the "latest" version of the zip file when deploying a new version.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -34,17 +34,21 @@ if [ "$PROCESS_DEVICES" == "" ]; then
 	PROCESS_DEVICES="$WOS_DEVICES"
 fi
 
+echo "Processing device list: $PROCESS_DEVICES..."
+
 for DEVICE in $PROCESS_DEVICES; do
 	# Find out which version of LineageOS we're going to build for this device.
 	WOS_BUILD_VAR=WOS_BUILD_VER_${DEVICE^^}
 	LOS_BUILD_VERSION=${!WOS_BUILD_VAR}
 
-	# A device name may have a special case where we're building multiple versios, like for LOS 16
+	# A device name may have a special case where we're building multiple versions, like for LOS 16
 	# and 17.  In these cases an extra modifier on the device name is added that starts with a '_'
 	# so for example dumpling_17 to indicate to build LOS 17 for dumpling.  In these cases we need
 	# to leave the modifier on $DEVICE so logs and other commands are executed in the right directory
 	# but for the acutal LOS build, we need to strip it off.  So do so now.
 	LOS_DEVICE=`echo $DEVICE | sed 's/_.*//'`
+
+	echo -n "    Deploying $LOS_DEVICE... "
 
 	# Change in to the device release directory.
 	cd ~/releases/ota/$LOS_DEVICE
@@ -52,17 +56,23 @@ for DEVICE in $PROCESS_DEVICES; do
 	# Build the packagename, but leave out the extension as we're moving multiple files.
 	PKGNAME=WundermentOS-$LOS_BUILD_VERSION-$TODAY-release-$LOS_DEVICE-signed
 
+	echo "$PKGNAME... "
+
 	# Make sure we have a package to deploy.
 	if [ -f $PKGNAME.zip ]; then
-		echo "Deploying $PKGNAME..."
 		# Use lftp to do the transfer, note we'll transfer the file up to the sever with a "piz" extension
-		# so taht the OTA updater doesn't pick it up and let users download it while it's uploading.
+		# so that the OTA updater doesn't pick it up and let users download it while it's uploading.
 		# We'll rename it at the very end of the process to "zip" so the OTA will find it only after completely
 		# all the transfers.
-		lftp sftp://$WOS_USER:$WOS_PASS@$WOS_HOST -e "set sftp:auto-confirm yes; cd $WOS_DIR_FULL; put $PKGNAME.zip -o $PKGNAME.piz; put $PKGNAME.zip.md5sum; put $PKGNAME.zip.prop; mv $PKGNAME.piz $PKGNAME.zip; bye"
+		# Also, create a link to the latest release for a given device in the form of "WundermentOS-device-latest.zip".
+		lftp sftp://$WOS_USER:$WOS_PASS@$WOS_HOST -e "set sftp:auto-confirm yes; cd $WOS_DIR_FULL; put $PKGNAME.zip -o $PKGNAME.piz; put $PKGNAME.zip.md5sum; put $PKGNAME.zip.prop; mv $PKGNAME.piz $PKGNAME.zip; rm WundermentOS-$LOS_DEVICE-latest.zip; ln -s $PKGNAME.zip WundermentOS-$LOS_DEVICE-latest.zip; bye"
 
 		# Save the date as the last release date for future use.
 		echo $TODAY > ~/devices/$LOS_DEVICE/status/last.release.date.txt
+		echo "    Done."
+	else
+		echo ""
+		echo "        Error deploying $LOS_DEVICE, no release package found!"
 	fi
 done
 
