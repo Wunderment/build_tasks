f23631d755def7b37ff99ef86a5a62529815f816
Greg <greg@toolstack.com>
2021-05-04 14:51:37 -0400

    Add UnifiedNlp in to the build system

---
+++ source/add-networklocation-apk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Go get the current NetworkLocation apk.
+cd ../unifiednlp
+./get-unifiednlp-apk.sh
+
+# Come back to the source directory.
+cd ../source
+
+# Update for all versions of LOS that we have.
+for LOSPATHNAME in ~/android/lineage-*; do
+	LOSDIRNAME=$(basename $LOSPATHNAME)
+
+	if [[ ! -d ~/android/$LOSDIRNAME/packages/apps/NetworkLocation ]]; 	then
+		echo -n "Adding UnifiedNLP to $LOSDIRNAME..."
+		cd ~/android/$LOSDIRNAME/packages/apps
+
+		mkdir NetworkLocation
+
+		cd NetworkLocation
+
+		cp ~/tasks/source/NetworkLocation-Android.mk Android.mk
+		cp ~/tasks/source/privapp_whitelist_unifiednlp.xml .
+		cp ~/tasks/unifiednlp/current-networklocation.apk NetworkLocation.apk
+
+		echo "done."
+	fi
+done
---
+++ unifiednlp/get-unifiednlp-apk.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# No script for this yet.
+exit;
+
+# Check to see if we've download the current apk in the last 240 minutes.
+if test `find "current-f-droid.apk" -mmin +240`
+then
+    echo Checking for new F-Droid apk...
+
+	# Get the web page for the f-droid apk.
+	wget -q https://f-droid.org/en/packages/org.fdroid.fdroid/
+
+	# Split the page based on the current suggested version.
+	csplit index.html '/name="suggested"/' > /dev/null
+
+	# We no longer need the index file.
+	rm index.html
+
+	# Split the page based on the download link section.
+	csplit xx01 '/package-version-download/' > /dev/null
+
+	# Split the page based on the Download text, this removes everything below the download URL for us.
+	csplit xx01 '/Download APK/' > /dev/null
+
+	# We no longer need the second hunk.
+	rm xx01
+
+	# Split the remaining lines in to separate files.
+	split -l 1 xx00 > /dev/null
+
+	# We no longer need the first hunk.
+	rm xx00
+
+	# The third line of the file has the url on it, so rename it for more processing.
+	mv xac new_url.txt
+
+	# We no longer need the other lines, so delete the files.
+	rm xa*
+
+	# Time to cleanup the new url file and get ride of stuff we don't need.
+	sed -i 's/\s*//' new_url.txt
+	sed -i 's/<a href="//' new_url.txt
+	sed -i 's/">//' new_url.txt
+
+	# We now compare the newly processed url with the last one we downloaded and see if they're different.
+	if ! diff new_url.txt url.txt > /dev/null; then
+		# We need to download a new apk, so import the url we retrieved earlier in to a variable.
+		FDROIDURL=$(<new_url.txt)
+
+		# Remove the old apk.
+		rm current-f-droid.apk
+
+		# Download the new apk.
+		wget -q -O current-f-droid.apk $FDROIDURL
+
+		# Replace the old url file with the new url file.
+		rm url.txt
+		mv new_url.txt url.txt
+	else
+	 	echo "F-Droid apk already up to date."
+	fi
+
+	# Touch the file so it has the current time/date on it.
+	touch current-f-droid.apk
+else
+	    echo "F-Droid apk already up to date (downloaded less than 240 minutes ago)."
+fi
