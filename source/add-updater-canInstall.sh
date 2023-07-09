797aeeecbad637d9045fcfcad6765eb5c32a51a2
Greg Ross <greg@toolstack.com>
2023-07-09 15:23:29 -0400

    Add support to patching newer updater files.

--- source/add-updater-canInstall.sh
+++ source/add-updater-canInstall.sh
@@ -11,11 +11,20 @@ for LOSPATHNAME in ~/android/lineage-*; do
 	# Update the building process for pre-built images other than vendor, only supported on LOS 17+.
 	if (( $LOSMAJOR >= 17 )); then
 		echo -n "Patching Updater for $LOSDIRNAME... "
-		cd $LOSPATHNAME/packages/apps/Updater/src/org/lineageos/updater/misc
+
+		# Newer versions of LineageOS store the source in a different path, so check to see which
+		# one we have and change to it.
+		if [ -d "$LOSPATHNAME/packages/apps/Updater/src/org/lineageos/updater/misc" ]; then
+			# LineageOS 20 July 2023 and older
+			cd $LOSPATHNAME/packages/apps/Updater/src/org/lineageos/updater/misc
+		else
+			# LineageOS 20 July 8th 2023 and newer
+			cd $LOSPATHNAME/packages/apps/Updater/app/src/main/java/org/lineageos/updater/misc
+		fi
 
 		# Check to see if we need to patch the updater before doing so.
 		if grep "SystemProperties.get(Constants.PROP_BUILD_VERSION));" Utils.java > /dev/null; then
-			patch Utils ~/tasks/source/updater-canInstall-$LOSVERSION.patch
+			patch Utils.java ~/tasks/source/updater-canInstall-$LOSVERSION.patch
 			echo "done."
 		else
 			echo "already patched."
