ea5aee4627bd8e791747db5ffcc873b9fa87fda1
Greg Ross <greg@toolstack.com>
2024-01-28 19:21:35 -0500

    Make sure to cleanup between devices.

--- build/build-all.sh
+++ build/build-all.sh
@@ -4,6 +4,7 @@
 source ~/.WundermentOS/devices.sh
 
 for DEVICE in $WOS_DEVICES; do
+	echo "Building $DEVICE..."
 	cd ~/devices/$DEVICE/build
-	./build.sh clean build sign log
+	./build.sh build sign log
 done
