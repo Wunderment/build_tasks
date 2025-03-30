ce400b7748c84d5aaf81fb68c0ed7dd87093a605
Greg Ross <greg@toolstack.com>
2025-03-30 14:49:42 -0400

    Update install script.

--- install/install.sh
+++ install/install.sh
@@ -21,11 +21,12 @@ git clone https://github.com/Wunderment/build_devices.git devices
 
 # Setup the android directory.
 cd ~/android
-mkdir lineage-17.1
-mkdir lineage-18.1
-mkdir lineage-19.1
+mkdir lineage-22.1
 
 # Setup the bin directory.
+if [ ! -d ~/bin ]; then
+	mkdir ~/bin
+fi
 cd ~/bin
 
 # Download sdat2img from https://github.com/xpirt/sdat2img.
@@ -51,51 +52,81 @@ echo "export WOS_HOST=\"\"" >> deploy-info.sh
 echo "export WOS_DIR_FULL=\"ota.wunderment.org/builds/full\"" >> deploy-info.sh
 echo "export WOS_GITHUB_REPO=\"releases\"" >> deploy-info.sh
 
-echo "export WOS_DEVICES=\"lemonade_18\"" > devices.sh
-echo "" > devices.sh
-echo "# OnePlus 5"
-echo "export WOS_BUILD_VER_DUMPLING_17=\"17.1\"" > devices.sh
+echo "export WOS_DEVICES=\"gts4lvwifi_22 oriole_22 panther_22 husky_22\"" > devices.sh
+echo "" >> devices.sh
+
+echo "# Configure which version of LOS is to be used for each device." >> devices.sh
+echo "" >> devices.sh
+echo "# OnePlus 5" >> devices.sh
+echo "export WOS_BUILD_VER_DUMPLING_17=\"17.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_DUMPLING_18=\"18.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_DUMPLING_19=\"19.1\"" >> devices.sh
-echo ""
-echo "# OnePlus 6"
+echo "export WOS_BUILD_VER_DUMPLING_20=\"20.0\"" >> devices.sh
+echo "" >> devices.sh
+echo "# OnePlus 6" >> devices.sh
 echo "export WOS_BUILD_VER_ENCHILADA_17=\"17.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_ENCHILADA_18=\"18.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_ENCHILADA_19=\"19.1\"" >> devices.sh
-echo ""
-echo "# OnePlus 6T"
+echo "export WOS_BUILD_VER_ENCHILADA_20=\"20.0\"" >> devices.sh
+echo "" >> devices.sh
+echo "# OnePlus 6T" >> devices.sh
 echo "export WOS_BUILD_VER_FAJITA_17=\"17.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_FAJITA_18=\"18.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_FAJITA_19=\"19.1\"" >> devices.sh
-echo ""
-echo "# OnePlus 7 Pro"
+echo "export WOS_BUILD_VER_FAJITA_20=\"20.0\"" >> devices.sh
+echo "" >> devices.sh
+echo "# OnePlus 7 Pro" >> devices.sh
 echo "export WOS_BUILD_VER_GUACAMOLE_18=\"18.1\"" >> devices.sh
-echo ""
-echo "# OnePlus 7"
+echo "" >> devices.sh
+echo "# OnePlus 7" >> devices.sh
 echo "export WOS_BUILD_VER_GUACAMOLEB_17=\"17.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_GUACAMOLEB_18=\"18.1\"" >> devices.sh
-echo ""
-echo "# OnePlus 8T"
+echo "" >> devices.sh
+echo "# OnePlus 8T" >> devices.sh
 echo "export WOS_BUILD_VER_KEBAB_18=\"18.1\"" >> devices.sh
-echo ""
-echo "# OnePlus 9"
+echo "" >> devices.sh
+echo "# OnePlus 9" >> devices.sh
 echo "export WOS_BUILD_VER_LEMONADE_18=\"18.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_LEMONADE_19=\"19.1\"" >> devices.sh
-echo ""
-echo "# Google Pixel 4"
+echo "" >> devices.sh
+echo "# Google Pixel 4" >> devices.sh
 echo "export WOS_BUILD_VER_FLAME_18=\"18.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_FLAME_19=\"19.1\"" >> devices.sh
-echo ""
-echo "# Google Pixel 6"
+echo "export WOS_BUILD_VER_FLAME_20=\"20.0\"" >> devices.sh
+echo "" >> devices.sh
+echo "# Google Pixel 5" >> devices.sh
 echo "export WOS_BUILD_VER_REDFIN_19=\"19.1\"" >> devices.sh
-echo ""
-echo "# Samsung Galaxy Tab S5e"
+echo "export WOS_BUILD_VER_REDFIN_20=\"20.0\"" >> devices.sh
+echo "" >> devices.sh
+echo "# Google Pixel 6" >> devices.sh
+echo "export WOS_BUILD_VER_ORIOLE_20=\"20.0\"" >> devices.sh
+echo "export WOS_BUILD_VER_ORIOLE_21=\"21.0\"" >> devices.sh
+echo "export WOS_BUILD_VER_ORIOLE_22=\"22.1\"" >> devices.sh
+echo "#export WOS_BUILD_VER_ORIOLE_22=\"22.2\"" >> devices.sh
+echo "" >> devices.sh
+echo "# Google Pixel 7" >> devices.sh
+echo "export WOS_BUILD_VER_PANTHER_20=\"20.0\"" >> devices.sh
+echo "export WOS_BUILD_VER_PANTHER_21=\"21.0\"" >> devices.sh
+echo "export WOS_BUILD_VER_PANTHER_22=\"22.1\"" >> devices.sh
+echo "" >> devices.sh
+echo "# Google Pixel 8 Pro" >> devices.sh
+echo "export WOS_BUILD_VER_HUSKY_21=\"21.0\"" >> devices.sh
+echo "export WOS_BUILD_VER_HUSKY_22=\"22.1\"" >> devices.sh
+echo "" >> devices.sh
+echo "# Google Pixel 9 Pro XL" >> devices.sh
+echo "export WOS_BUILD_VER_KOMODO_22=\"22.1\"" >> devices.sh
+echo "" >> devices.sh
+echo "# Samsung Galaxy Tab S5e" >> devices.sh
 echo "export WOS_BUILD_VER_GTS4LVWIFI_17=\"17.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_GTS4LVWIFI_18=\"18.1\"" >> devices.sh
 echo "export WOS_BUILD_VER_GTS4LVWIFI_19=\"19.1\"" >> devices.sh
-echo ""
-echo "# Android Studio Emulator"
+echo "export WOS_BUILD_VER_GTS4LVWIFI_20=\"20.0\"" >> devices.sh
+echo "export WOS_BUILD_VER_GTS4LVWIFI_21=\"21.0\"" >> devices.sh
+echo "export WOS_BUILD_VER_GTS4LVWIFI_22=\"22.1\"" >> devices.sh
+echo "" >> devices.sh
+echo "# Android Studio Emulator" >> devices.sh
 echo "export WOS_BUILD_VER_LINEAGE_X86_17=\"17.1\"" >> devices.sh
+echo "" >> devices.sh
 
 echo "export WOS_LOGDEST=\"root\"" > log-email-address.sh
 
